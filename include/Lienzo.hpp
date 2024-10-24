5f7cbcb3ac97a05bdbb75ca6d3759963782ae5c7
ErickRG2004 <a23310402@live.ceti.mx>
2024-10-24 01:56:29 +0000

    clase 3

--- include/Lienzo.hpp
+++ include/Lienzo.hpp
@@ -1,10 +1,24 @@
 #pragma once
+#include <Vector.hpp>
+#include <SerieNavidad.hpp>
+
 
 class Lienzo
 {
 private:
-    
+    Vector posicion; //codigo tiene
+    Vector tamano;
 public:
-    Lienzo() {}
+    Lienzo(Vector p, Vector t) {
+        this->posicion=p;
+        this->tamano=t;
+    }
     ~Lienzo() {}
+
+    void Dibujar(SerieNavidad serie){ //codigo de usa, recibe el objeto y lo usa
+        serie.Imprimir();
+    }
+    void Limpiar(){
+        system("clear");
+    }
 };
--- main.cpp
+++ main.cpp
@@ -1,14 +1,35 @@
-#include <iostream>
-#include <Foco.hpp>
+#include <SerieNavidad.hpp>
+#include <Lienzo.hpp>
+#include <Vector.hpp>
+#include <unistd.h>
 
 int main(int argc, char const *argv[])
 {
-    Foco foco1;
-    Foco foco2;
+     Vector p(0, 5);
+     Vector t(100, 100);
 
-    std:: cout<<"Encendiendo foco"<<std::endl;
-    foco1.Encender();
-    std:: cout<<"Apagando foco"<<std::endl;
-    foco1.Apagar();
-    return 0;
+     Lienzo lienzo(p, t);
+     SerieNavidad serie1(20);
+     SerieNavidad serie2(20);
+     bool estado = true;
+     int incremento=0;
+     while (true)
+     {
+          //Celula que explota
+          serie1.Alternar(estado);
+          lienzo.Dibujar(serie1);
+          estado = estado ? false : true;
+
+          //Incremento
+
+          serie2.Apagar();
+          serie2.Encender(5);
+          lienzo.Dibujar(serie2);
+          incremento++;
+
+          sleep(1);
+          lienzo.Limpiar();
+     }
+
+     return 0;
 }
